0eee18758f1b9b8ce3c65c67e24819cfcfe97e13
Legoviv <93427869+Legoviv@users.noreply.github.com>
2022-03-07 20:08:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -27,3 +27,69 @@ function uploadBackground(){
 function uploadrover(){
     ctx.drawImage(rover_imgtag , rover_x , rover_y , rover_width , rover_height);
 }
+
+window.addEventListener("keydown",my_keydown);
+
+
+function my_keydown(e){
+    keyPressed=e.keyCode;
+
+    console.log(keyPressed);
+
+    if(keyPressed=='37'){
+        left();
+        console.log("left");
+    }
+
+    if(keyPressed=='38'){
+        up();
+        console.log("up");
+    }
+
+    if(keyPressed=='39'){
+        right();
+        console.log("right");
+    }
+
+    if(keyPressed=='40'){
+        down();
+        console.log("down");
+    }
+
+}
+
+function up(){
+    if(rover_y>=0){
+        rover_y=rover_y-10;
+        console.log("when up arrow is pressed , x="+rover_x+"y="+rover_y);
+        uploadBackground();
+        uploadrover();
+    }
+}
+
+function down(){
+    if(rover_y<=510){
+        rover_y=rover_y+10;
+        console.log("when down arrow is pressed , x="+rover_x+"y="+rover_y);
+        uploadBackground();
+        uploadrover();
+    }
+}
+
+function left(){
+    if(rover_x>=0){
+        rover_x=rover_x-10;
+        console.log("when left arrow is pressed , x="+rover_x+"y="+rover_y);
+        uploadBackground();
+        uploadrover();
+    }
+}
+
+function right(){
+    if(rover_x<=700){
+        rover_x=rover_x+10;
+        console.log("when right arrow is pressed , x="+rover_x+"y="+rover_y);
+        uploadBackground();
+        uploadrover();
+    }
+}
